1cfa8821d334367fd4102e9facc73a77474067bd
anajuliasilvaa <anajudasilvaalmeida@email.com>
2025-07-06 19:15:27 -0300

    Adiciona funcionalidades de gerenciamento de perfil de usuário, incluindo alteração de senha e escolha de avatar. Implementa sistema de redefinição de senha por email e atualiza templates para melhorar a experiência do usuário.

--- catalogofilmes/catalogofilmes/settings.py
+++ catalogofilmes/catalogofilmes/settings.py
@@ -131,3 +131,12 @@ LOGIN_URL = '/usuaios/login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'anajudasilvaalmeida@gmail.com'
+EMAIL_HOST_PASSWORD = 'szfj grsr jtuj zncm'
+DEFAULT_FROM_EMAIL = 'anajudasilvaalmeida@gmail.com'
